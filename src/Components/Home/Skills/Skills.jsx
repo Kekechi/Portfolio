cd83384c4ea5d07feea6919bdee9e6a6982931e9
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2023-12-22 10:56:32 -0800

    skill section background

--- src/Components/Home/Skills/Skills.jsx
+++ src/Components/Home/Skills/Skills.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function Skills() {
-  return <div>Skills</div>;
+  return (
+    <div id="skills" className="skills">
+      <Container>Skills</Container>
+    </div>
+  );
 }
 
 export default Skills;
