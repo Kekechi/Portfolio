ac7850ae6e6506418651368907462bc57b3a5d91
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2023-12-22 20:00:22 -0800

    add skills

--- src/Components/Home/Skills/Skills.jsx
+++ src/Components/Home/Skills/Skills.jsx
@@ -1,10 +1,68 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import skillLists from "./skillLists";
 
 function Skills() {
   return (
     <div id="skills" className="skills">
-      <Container>Skills</Container>
+      <Container className="py-5">
+        <h1 className="text-light">Skills</h1>
+
+        <Row>
+          <Col sm={12} md={6}>
+            <h2 className="text-light pt-2">Programming Languages</h2>
+            <Row>
+              {skillLists.languages.map((language, index) => (
+                <Col key={index} sm={6} md={12} lg={6}>
+                  <li className="text-light skill-items text-nowrap">
+                    {language}
+                  </li>
+                </Col>
+              ))}
+            </Row>
+            <h2 className="text-light pt-2">Frameworks</h2>
+            <Row>
+              {skillLists.frameworks.map((framework, index) => (
+                <Col key={index} sm={6} md={12} lg={6}>
+                  <li className="text-light skill-items text-nowrap">
+                    {framework}
+                  </li>
+                </Col>
+              ))}
+            </Row>
+            <h2 className="text-light pt-2">Database</h2>
+            <Row>
+              {skillLists.databases.map((database, index) => (
+                <Col key={index} sm={6} md={12} lg={6}>
+                  <li className="text-light skill-items text-nowrap">
+                    {database}
+                  </li>
+                </Col>
+              ))}
+            </Row>
+            <h2 className="text-light pt-2">Others</h2>
+            <Row>
+              {skillLists.others.map((other, index) => (
+                <Col key={index} sm={6} md={12} lg={6}>
+                  <li className="text-light skill-items  text-nowrap">
+                    {other}
+                  </li>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+          <Col sm={12} md={6}>
+            <h2 className="text-light pt-2">Tools</h2>
+            <Row>
+              {skillLists.tools.map((tool, index) => (
+                <Col key={index} sm={6} md={12} lg={6}>
+                  <li className="text-light skill-items text-nowrap">{tool}</li>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/Components/Home/Skills/skillLists.js
@@ -0,0 +1,25 @@
+const skillLists = {
+  languages: ["Java", "JavaScript", "Python", "HTML", "CSS"],
+  frameworks: ["React", "Node.js"],
+  databases: ["MongoDB"],
+  tools: [
+    "Git",
+    "GitHub",
+    "VS Code",
+    "Postman",
+    "AWS EC2",
+    "AWS S3",
+    "WireShark",
+    "HashCat",
+    "Burp Suite",
+    "Nmap",
+    "Metasploit",
+    "Kali Linux",
+    "Ubuntu",
+    "Windows",
+    "Windows Server",
+  ],
+  others: ["English", "Japanese"],
+};
+
+export default skillLists;
