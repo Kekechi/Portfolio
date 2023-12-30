d21e694d9a6796ee642bca28861a6b55105a7835
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2023-12-30 15:53:50 -0800

    create list of experience texts

--- src/Components/Home/Experience/Experience.jsx
+++ src/Components/Home/Experience/Experience.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+
+import experienceList from "./experienceList";
 
 function Experience() {
   return (
@@ -7,85 +9,24 @@ function Experience() {
       <div className="section-anchor" id="experience"></div>
       <Container className="py-5">
         <h1 className="text-light">Experience</h1>
+
         <Row>
-          <Col lg={4}>
-            <div className="experience-card">
-              <div className="experience-card-background">
-                <img src="src/assets/img/cdt.png" alt="" />
-              </div>
+          {experienceList.map((experience) => (
+            <Col lg={4} key={experience.title}>
+              <div className="experience-card">
+                <div className="experience-card-background">
+                  <img src={"src/assets/img/" + experience.img} alt="" />
+                </div>
 
-              <div className="experience-card-content">
-                <h6>Position: Member</h6>
-                <p>September 2023 - Present</p>
-                <p>
-                  Expand knowledge involving preserving confidentiality,
-                  integrity, and availability of information. Information
-                  security professionals and students with experience in
-                  multiple cybersecurity fields provide instruction. Chosen to
-                  compete at the CCDC invitational on 11/4/23.
-                </p>
+                <div className="experience-card-content">
+                  <h6>{experience.position}</h6>
+                  <p>{experience.date}</p>
+                  <p>{experience.description}</p>
+                </div>
+                <h4 className="experience-title">{experience.title}</h4>
               </div>
-              <h4 className="experience-title">Cyber Defense Team</h4>
-            </div>
-          </Col>
-          <Col lg={4}>
-            <div className="experience-card">
-              <div className="experience-card-background">
-                <img src="src/assets/img/newspicks.png" alt="" />
-              </div>
-
-              <div className="experience-card-content">
-                <h6>Position: 5th Gen Student Picker</h6>
-                <p>September 2023 - Present</p>
-                <p>
-                  NewsPicks aggregates news from 90 global sources and presents
-                  insights from 300+ "pro-pickers" and "student pickers" to its
-                  7.9 million users. Comment on news and original articles, and
-                  collaborate closely with the community team.
-                </p>
-              </div>
-              <h4 className="experience-title">NewsPicks</h4>
-            </div>
-          </Col>
-          <Col lg={4}>
-            <div className="experience-card">
-              <div className="experience-card-background">
-                <img src="src/assets/img/skgoicon.png" alt="" />
-              </div>
-
-              <div className="experience-card-content">
-                <h6>Position: Volunteer Leader / Founder</h6>
-                <p>May 2022 - Present</p>
-                <p>
-                  Founded a student volunteer organization to counter the
-                  decline in Japan-U.S. cultural exchange among youth. As a
-                  representative, led efforts to immerse American children in
-                  Japanese culture and language, enhancing global understanding.
-                </p>
-              </div>
-              <h4 className="experience-title">SKGOJapanese</h4>
-            </div>
-          </Col>
-          <Col lg={4}>
-            <div className="experience-card">
-              <div className="experience-card-background">
-                <img src="src/assets/img/patribots.png" alt="" />
-              </div>
-              <div className="experience-card-content">
-                <h6>Position: Programming Lead</h6>
-                <p>September 2020 - June 2023</p>
-                <p>
-                  Led a team of 8 in Java-based robot programming, integrating
-                  everyone's contributions. Cultivated team-building skills,
-                  ensuring a cohesive and harmonious work environment. Achieved
-                  the Imagery Award at the Las Vegas regional competition.
-                </p>
-              </div>
-              <h4 className="experience-title">
-                FRC Robotics Team 4738 Patribots
-              </h4>
-            </div>
-          </Col>
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
---
+++ src/Components/Home/Experience/experienceList.js
@@ -0,0 +1,44 @@
+const experienceList = [
+  {
+    title: "Cyber Defense Team",
+    position: "Member",
+    date: "September 2023 - Present",
+    description:
+      "Expand knowledge involving preserving confidentiality,integrity, and availability of information. Information security professionals and students with experience in multiple cybersecurity fields provide instruction. Chosen to compete at the CCDC invitational on 11/4/23.",
+    img: "cdt.png",
+  },
+  {
+    title: "NewsPicks",
+    position: "5th Gen Student Picker",
+    date: "September 2023 - Present",
+    description:
+      'NewsPicks aggregates news from 90 global sources and presents insights from 300+ "pro-pickers" and "student pickers" to its 7.9 million users. Comment on news and original articles, and collaborate closely with the community team.',
+    img: "newspicks.png",
+  },
+  {
+    title: "SKGOJapanese",
+    position: "Volunteer Leader / Founder",
+    date: "May 2022 - Present",
+    description:
+      "Founded a student volunteer organization to counter the decline in Japan-U.S. cultural exchange among youth. As a representative, led efforts to immerse American children in Japanese culture and language, enhancing global understanding.",
+    img: "skgoicon.png",
+  },
+  {
+    title: "Gardena Valley Japanese Cultural Institute",
+    position: "IT Assistance Volunteer",
+    date: "March 2020 - Present",
+    description:
+      "Advised students on online learning tools and strategies. Assisted with instructional activities and assessments in virtual classes.",
+    img: "jci.png",
+  },
+  {
+    title: "FRC Robotics Team 4738 Patribots",
+    position: "Programming Lead",
+    date: "September 2020 - June 2023",
+    description:
+      " Led a team of 8 in Java-based robot programming, integrating everyone's contributions. Cultivated team-building skills, ensuring a cohesive and harmonious work environment. Achieved the Imagery Award at the Las Vegas regional competition.",
+    img: "patribots.png",
+  },
+];
+
+export default experienceList;
