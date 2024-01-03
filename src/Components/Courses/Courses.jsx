c7e6121b6fa675dc27ff61a60f9dede4ed1af37c
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2024-01-02 16:35:17 -0800

    Update Courses.jsx

--- src/Components/Courses/Courses.jsx
+++ src/Components/Courses/Courses.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+
+import courseList from "./coursesList";
 
 function Courses() {
-  return <div>Courses</div>;
+  return (
+    <div className="courses m-nav">
+      <Container className="py-5">
+        <h1 className="text-light">Courses</h1>
+        {courseList.map((sections) => (
+          <div key={sections.section} className="text-light">
+            <h2>{sections.section}</h2>
+            <ul>
+              {sections.courses.map((course) => (
+                <li key={course.name}>
+                  {course.name} {course?.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </Container>
+    </div>
+  );
 }
 
 export default Courses;
