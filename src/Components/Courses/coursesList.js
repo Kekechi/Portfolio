5a1c610ebf098d2ec22f3c6a0b36b34df97e9a31
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2024-01-02 16:35:10 -0800

    create courses page

---
+++ src/Components/Courses/coursesList.js
@@ -0,0 +1,52 @@
+const courseList = [
+  {
+    section: "Computer Science",
+    courses: [
+      { name: "CS 160", title: "Intermediate Computer Programming" },
+      {
+        name: "CS 160L",
+        title: "Intermediate Computer Programming Laboratory",
+      },
+    ],
+  },
+  {
+    section: "Mathematics",
+    courses: [{ name: "MATH 254", title: "Introduction to Linear Algebra" }],
+  },
+  {
+    section: "Interdisciplinary Studies",
+    courses: [
+      {
+        name: "HONOR 113",
+        title:
+          "Seminar in Honors Connection and Commitment (Applied Creativity)",
+      },
+    ],
+  },
+  {
+    section: "General Education",
+    courses: [{ name: "PHIL 140", title: "Technology and Human Behavior" }],
+  },
+  {
+    section: "High School ",
+    courses: [
+      { name: "AP Computer Science Principle" },
+      { name: "AP Computer Science A" },
+      { name: "AP Calculus AB" },
+      { name: "AP Calculus BC" },
+      { name: "AP Physics C: Mechanics" },
+      {
+        name: "MFET101",
+        title: "Introduction to Manufacturing Engineering Technology",
+      },
+      { name: "AP Statistics" },
+      { name: "COMS103", title: "Oral Communication " },
+      { name: "ENGL101", title: "Reading and Composition" },
+      { name: "AP Art History" },
+      { name: "AP United States Government and Politics" },
+      { name: "AP Japanese" },
+    ],
+  },
+];
+
+export default courseList;
