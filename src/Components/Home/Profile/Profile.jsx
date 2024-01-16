9aa4793d4f65ad8662e7634362be7328f9815384
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2024-01-15 17:38:37 -0800

    animate

--- src/Components/Home/Profile/Profile.jsx
+++ src/Components/Home/Profile/Profile.jsx
@@ -1,24 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import profileTexts from "./profileTexts";
 import LangContext from "../../../Context/LangContext";
+import { InView } from "react-intersection-observer";
 
 function Profile() {
   const lang = useContext(LangContext);
   const profileTextsObj = profileTexts[lang];
+  const [show, setShow] = useState(false);
 
   return (
-    <div className="profile">
+    <InView
+      onChange={(inView, entry) => setShow(inView)}
+      threshold={0.1}
+      className="profile"
+    >
       <div className="section-anchor" id="profile"></div>
       <Container className="py-5">
-        <h1 className="text-light">{profileTextsObj.title}</h1>
+        <div></div>
+        <h1 className={"text-light typeIn " + (show ? "show" : "")}>
+          {profileTextsObj.title}
+        </h1>
         <Row className="pt-5">
           <Col xs={12} lg={4}>
-            <div className="profile-pic">
+            <div className={"profile-pic fadeIn " + (show ? "show" : "")}>
               <img src="/src/assets/img/profile.JPG" alt="" />
             </div>
           </Col>
-          <Col xs lg={8}>
+          <Col xs lg={8} className={"flyInRight " + (show ? "show" : "")}>
             <h2 className="text-light">{profileTextsObj.name}</h2>
             <p className="text-light">
               {profileTextsObj.description}
@@ -29,7 +38,7 @@ function Profile() {
           </Col>
         </Row>
       </Container>
-    </div>
+    </InView>
   );
 }
 
