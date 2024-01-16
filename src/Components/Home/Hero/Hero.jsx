6cf047ec8b0ffff12402e68650c32a1001ec7e39
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2024-01-15 17:38:16 -0800

    support Japanese

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container } from "react-bootstrap";
+import heroTexts from "./heroTexts";
+import LangContext from "../../../Context/LangContext";
 
 export const Hero = () => {
+  const lang = useContext(LangContext);
+  const heroTextsObj = heroTexts[lang];
+
   return (
     <div className="hero d-flex flex-column justify-content-center" id="top">
       <Container>
-        <h1 className="text-light title">Keigo Morita</h1>
+        <h1 className="text-light title">{heroTextsObj.name}</h1>
         <p className="text-light headline">
-          San Diego State University
-          <br /> Computer Science Major
-          <br /> Weber Honors College
+          {heroTextsObj.university}
+          <br /> {heroTextsObj.major}
+          <br /> {heroTextsObj.honors}
         </p>
       </Container>
     </div>
---
+++ src/Components/Home/Hero/heroTexts.js
@@ -0,0 +1,16 @@
+const heroTexts = {
+  en: {
+    name: "Keigo Morita",
+    university: "San Diego State University",
+    major: "Computer Science Major",
+    honors: "Weber Honors College",
+  },
+  ja: {
+    name: "守田 圭吾",
+    university: "サンディエゴ州立大学",
+    major: "コンピュータサイエンス専攻",
+    honors: "ウェーバー・オナーズ・カレッジ",
+  },
+};
+
+export default heroTexts;
